e31cdaeeffc81a9323506ac13bfd4ad39b17d471
rishabh1b <biyanirishabh@gmail.com>
2017-10-10 14:18:30 -0700

    [inst] OpenCV integration with the boilerplate

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,35 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+ * @file main.cpp
+ * @brief Toy function to check openCV integration
+ * @author Rishabh Biyani(rishabh1b)
+ * @copyright GNU Public License
+ *
+ */
+/* #include <cv.h>
+ #include <highgui.h> */
 
-int main()
-{
-    dummy();
-    return 0;
+#include "opencv2/opencv.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+// using namespace cv;
+
+int main() {
+  // dummy();
+  std::string filename = "data/cornflakesCropped.png";
+  cv::Mat image;
+  image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+
+  if (!image.data) {
+    printf("No image data \n");
+    return -1;
+  }
+
+  cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+  imshow("Display Image", image);
+
+  cv::waitKey(0);
+
+  return 0;
 }
