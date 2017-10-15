bbd442d8bc70054783d1b4409bf4b6b15ae47d6e
rishabh1b <biyanirishabh@gmail.com>
2017-10-15 15:45:13 -0700

    [impl] Video Frame Class implemented and working fine

--- app/main.cpp
+++ app/main.cpp
@@ -12,24 +12,56 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "VideoFrame.h"
 
 // using namespace cv;
 
 int main() {
   // dummy();
-  std::string filename = "data/cornflakesCropped.png";
-  cv::Mat image;
-  image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+  /* std::string filename = "data/cornflakesCropped.png";
+   cv::Mat image;
+   image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
 
-  if (!image.data) {
-    printf("No image data \n");
-    return -1;
-  }
+   if (!image.data) {
+   printf("No image data \n");
+   return -1;
+   }
 
-  cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
-  imshow("Display Image", image);
+   cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+   imshow("Display Image", image);
 
-  cv::waitKey(0);
+   cv::waitKey(0);*/
+
+  VideoFrame vidFrame = VideoFrame("data/demo.mp4");
+  cv::Mat frame = vidFrame.nextFrame();
+
+   if (!frame.data) {
+   printf("No image data \n");
+   return -1;
+   }
+
+
+   cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+   cv::imshow("Display Image", frame);
+
+   cv::waitKey(0);
+//  std::string filename = "data/demo.mp4";
+//  cv::VideoCapture cap(filename);  // open the default camera
+//  if (!cap.isOpened())  // check if we succeeded
+//    return -1;
+//
+//  cv::Mat frame;
+//  cap >> frame;
+//
+//  if (!frame.data) {
+//    printf("No image data \n");
+//    return -1;
+//  }
+//
+//  cv::namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+//  cv::imshow("Display Image", frame);
+//
+//  cv::waitKey(0);
 
   return 0;
 }
---
+++ include/VideoFrame.h
@@ -0,0 +1,60 @@
+/*
+ * @file VideoFrame.h
+ * @author Rishabh Biyani(rishabh1b)
+ * @copyright MIT license
+ *  Created on: Oct 15, 2017
+ */
+#include <string>
+#include "opencv2/opencv.hpp"
+
+#ifndef INCLUDE_VIDEOFRAME_H_
+#define INCLUDE_VIDEOFRAME_H_
+
+class VideoFrame {
+
+ public:
+  /**
+   * @brief VideoFrame constructor
+   * @param filename a string specifying path to the video file
+   */
+  VideoFrame(std::string filename);
+
+  /**
+   * @brief Video Frame Destructor. It takes care
+   * of releasing VideoCapture Object
+   */
+  ~VideoFrame() {
+    this->vidcapture.release();
+  }
+
+  /**
+   * @brief nextFrame member function
+   * @return img a cv::Mat instance
+   */
+
+  cv::Mat nextFrame();
+
+  /**
+   * @brief setFilename member function
+   * @param filename a string specifying path to a video file
+   */
+  void setFilename(std::string filename);
+
+  /**
+   * Test function
+   */
+  void test();
+
+ private:
+  /**
+   * OpenCv videoCapture Object
+   */
+  cv::VideoCapture vidcapture;
+  /**
+   *filename of the video clip currently being parsed
+   */
+  std::string filename;
+
+};
+
+#endif /* INCLUDE_VIDEOFRAME_H_ */
